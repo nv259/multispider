780a024030973b4fb3dfd4cfb31fac4591090b06
nv259 <nguyenvietnhat.03@gmail.com>
2024-04-21 15:15:41 +0700

    initialize architecture of dema

---
+++ duorat/models/dema.py
@@ -0,0 +1,41 @@
+# Inter Deep Ensemble Model-Agnostic
+import logging
+
+import torch
+
+from duorat.models.rat import RATLayer
+from duorat.models.lm_duorat import LMDuoRATModel
+from duorat.utils import registry
+
+logger = logging.getLogger(__name__)
+
+
+@registry.register("model", "InterDEMA")
+class InterDEMA(LMDuoRATModel):
+    """
+    A LM DuoRAT model with first layer is clone multiple times 
+    """ 
+    def __init__(self, preprocess, encoder, decoder, num_particles=5):
+        super().__init__(
+            preproc=preprocess,
+            encoder=encoder,
+            decoder=decoder
+        )
+        
+        self.num_particles = num_particles
+         
+        self.list_first_rats = torch.nn.ModuleList()
+        for _ in range(num_particles):
+            particle_encoder = RATLayer(
+                embed_dim=self.decoder_rat_embed_dim,
+                mem_embed_dim=self.mem_embed_dim,
+                num_heads=decoder["rat_num_heads"],
+                ffn_dim=decoder["rat_ffn_dim"],
+                dropout=decoder["rat_dropout"],
+                attention_dropout=decoder["rat_attention_dropout"],
+                relu_dropout=decoder["rat_relu_dropout"]
+            )
+            
+            self.list_first_rats.append(particle_encoder)
+        
+        
