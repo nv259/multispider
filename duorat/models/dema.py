3d64dd3beacccef2de98cafbef624cad13cce8d4
nv259 <nguyenvietnhat.03@gmail.com>
2024-04-22 15:30:55 +0700

    check code convention

--- duorat/models/dema.py
+++ duorat/models/dema.py
@@ -2,9 +2,8 @@
 import logging
 
 import torch
-
-from duorat.models.rat import RATLayer
 from duorat.models.lm_duorat import LMDuoRATModel
+from duorat.models.rat import RATLayer
 from duorat.utils import registry
 
 logger = logging.getLogger(__name__)
@@ -13,21 +12,17 @@ logger = logging.getLogger(__name__)
 @registry.register("model", "InterDEMA")
 class InterDEMA(LMDuoRATModel):
     """
-    A LM DuoRAT model with first layer is clone multiple times 
-    """ 
+    A LM DuoRAT model with first layer is clone multiple times
+    """
+
     def __init__(self, preprocess, encoder, decoder, num_particles=5):
-        
-        # ascertain the number of encoder remain unchanged
-        encoder["rat_num_layers"] = encoder["rat_num_layer"] - 1 
-        
-        super().__init__(
-            preproc=preprocess,
-            encoder=encoder,
-            decoder=decoder
-        )
-        
+        # ascertain the number of encoder layers remain unchanged
+        encoder["rat_num_layers"] = encoder["rat_num_layer"] - 1
+
+        super().__init__(preproc=preprocess, encoder=encoder, decoder=decoder)
+
         self.num_particles = num_particles
-         
+
         self.list_first_rats = torch.nn.ModuleList()
         for _ in range(num_particles):
             particle_encoder = RATLayer(
@@ -37,9 +32,7 @@ class InterDEMA(LMDuoRATModel):
                 ffn_dim=decoder["rat_ffn_dim"],
                 dropout=decoder["rat_dropout"],
                 attention_dropout=decoder["rat_attention_dropout"],
-                relu_dropout=decoder["rat_relu_dropout"]
+                relu_dropout=decoder["rat_relu_dropout"],
             )
-            
+
             self.list_first_rats.append(particle_encoder)
-        
-        
