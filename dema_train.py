30dc0c9d49e6b6065557f91f9c9940ead14457fd
nv259 <nguyenvietnhat.03@gmail.com>
2024-04-23 15:30:56 +0700

    implement Inter Deep Ensemble Model Agnostic method

--- dema_train.py
+++ dema_train.py
@@ -1,119 +1,326 @@
 import argparse
-import functools
 import collections
 import datetime
+import functools
 import itertools
 import json
 import os
 import traceback
-from typing import Type, List
+from typing import List, Type
 
 import _jsonnet
-import torch
 import numpy as np
+import torch
 
 # noinspection PyUnresolvedReferences
-from torch.cuda.amp import autocast, GradScaler
+from torch.cuda.amp import GradScaler, autocast
 from torch.utils.data import DataLoader
 
 # noinspection PyUnresolvedReferences
-from duorat import datasets
-
 # noinspection PyUnresolvedReferences
-from duorat import preproc
-
 # noinspection PyUnresolvedReferences
-from duorat import models
+from duorat import datasets, models, preproc
 
 # noinspection PyUnresolvedReferences
 from duorat.asdl.lang.spider.spider_transition_system import SpiderTransitionSystem
 from duorat.types import RATPreprocItem
 
 # noinspection PyUnresolvedReferences
-from duorat.utils import optimizers
-from duorat.utils import registry, parallelizer
-from duorat.utils import random_state
+from duorat.utils import optimizers, parallelizer, random_state, registry
 from duorat.utils import saver as saver_mod
 from duorat.utils.evaluation import evaluate_default, load_from_lines
-from third_party.spider.evaluation import LEVELS
 from preprocess import Preprocessor
-
+from third_party.spider.evaluation import LEVELS
 from train import Logger, Trainer
 
 
-class DEMATrainer(Trainer):    
+class DEMATrainer(Trainer):
     def load_train_config(self):
         # TODO: inspect config file
-        
-        if self.train_config.n_grad_accumulation_steps > 1: 
+
+        if self.train_config.n_grad_accumulation_steps > 1:
             self.logger.warn("Batch accumulation is used only at MAML-step level")
             raise NotImplementedError
-   
-    @staticmethod
-    def get_kernel(params: torch.Tensor, num_particles):
-        pass
-    
-    @staticmethod
-    def get_kernel_wSGLD_B(params: torch.Tensor, num_particles):
-        pass
-    
-    @staticmethod
-    def get_pairwise_distance_matrix(x: torch.Tensor) -> torch.Tensor:
-        pass
-    
-    @staticmethod
-    def vector_to_list_params(vector, other_params):
-        pass
-    
+
+    def get_kernel(self, params: torch.Tensor, num_particles):
+        """
+        Compute the RBF kernel for the input
+
+        Args:
+            params: a tensor of shape (N, M)
+
+        Returns: kernel_matrix = tensor of shape (N, N)
+        """
+        pairwise_d_matrix = self.get_pairwise_distance_matrix(x=params)
+
+        median_dist = torch.quantile(
+            input=pairwise_d_matrix, q=0.5
+        )  # tf.reduce_mean(euclidean_dists) ** 2
+        h = median_dist / np.log(num_particles)
+
+        kernel_matrix = torch.exp(-pairwise_d_matrix / h)
+        kernel_sum = torch.sum(input=kernel_matrix, dim=1, keepdim=True)
+        grad_kernel = -torch.matmul(kernel_matrix, params)
+        grad_kernel += params * kernel_sum
+        grad_kernel /= h
+
+        return kernel_matrix, grad_kernel, h
+
+    def get_kernel_wSGLD_B(self, params: torch.Tensor, num_particles):
+        """
+        Compute the RBF kernel and repulsive term for wSGLD
+
+        Args:
+            params: a tensor of shape (N, M)
+
+        Returns:
+            - kernel_matrix = tensor of shape (N, N)
+            - repulsive term
+        """
+        pairwise_d_matrix = DEMATrainer.get_pairwise_distance_matrix(x=params)
+        median_dist = torch.quantile(input=pairwise_d_matrix, q=0.5)
+        h = median_dist / np.log(num_particles)
+        kernel_matrix = torch.exp(-pairwise_d_matrix / h)
+        kernel_sum = torch.sum(input=kernel_matrix, dim=1, keepdim=True)
+        # compute repulsive term of w_SGLD_B
+        # invert of kernel_sum Nx1
+        invert_kernel_sum = kernel_sum.pow_(-1)
+        grad_kernel = params * (
+            torch.matmul(kernel_matrix, invert_kernel_sum)
+            + torch.sum(kernel_matrix * invert_kernel_sum, dim=1, keepdim=True)
+        )
+        grad_kernel += -(
+            torch.matmul(
+                kernel_matrix * torch.transpose(invert_kernel_sum, 0, 1), params
+            )
+            + torch.matmul(kernel_matrix, params) * invert_kernel_sum
+        )
+        grad_kernel /= h
+
+        return kernel_matrix, grad_kernel, h
+
+    def get_pairwise_distance_matrix(self, x: torch.Tensor) -> torch.Tensor:
+        """Calculate the pairwise distance between each row of tensor x
+
+        Args:
+            x: input tensor
+
+        Return: matrix of point-wise distances
+        """
+        n, m = x.shape
+
+        # initialize matrix of pairwise distances as a N x N matrix
+        pairwise_d_matrix = torch.zeros(size=(n, n), device=x.device)
+
+        # num_particles = particle_tensor.shape[0]
+        euclidean_dists = torch.nn.functional.pdist(input=x, p=2)  # shape of (N)
+
+        # assign upper-triangle part
+        triu_indices = torch.triu_indices(row=n, col=n, offset=1)
+        pairwise_d_matrix[triu_indices[0], triu_indices[1]] = euclidean_dists
+
+        # assign lower-triangle part
+        pairwise_d_matrix = torch.transpose(pairwise_d_matrix, dim0=0, dim1=1)
+        pairwise_d_matrix[triu_indices[0], triu_indices[1]] = euclidean_dists
+
+        return pairwise_d_matrix
+
+    def vector_to_list_params(self, vector, other_params):
+        params = []
+
+        # pointer for each layer params
+        pointer = 0
+
+        for param in other_params:
+            # total number of params each layer
+            num_params = int(np.prod(param.shape))
+
+            params.append(vector[pointer : pointer + num_params].view(param.shape))
+
+            pointer += num_params
+
+        return params
+
     def ensemble_train(self, batch, prior_scale=1e-3):
         assert self.model.training
-        
+
         inner_inter_params = []
         for i in range(self.num_particles):
             inner_inter_params.append(list(self.model.list_first_rat[i].parameters()))
-            
+
         params_matrix = torch.stack(
-            [torch.nn.utils.parameters_to_vector(params) for params in inner_inter_params],
-            dim=0
+            [
+                torch.nn.utils.parameters_to_vector(params)
+                for params in inner_inter_params
+            ],
+            dim=0,
         )
-        
-        pass
-        
-    def _update(self, train_data_loader, optimizer, lr_scheduler, scaler, saver, modeldir, last_step, prior_scale=1e-3):
+
+        initial_encoder_len = len(list(self.model.initial_encoder.parameters()))
+        particle_len = len(
+            list(self.model.list_first_rats[0].parameters())
+        )  # TODO: ascertain this
+        encoder_len = len(list(self.model.encoder_rat_layers.parameters()))
+
+        ret_dict = {}
+
+        # For computing distance
+        distance_nll = torch.empty(
+            size=(self.num_particles, params_matrix.size(1)),
+            device=next(self.model.parameters()).device,
+        )
+
+        final_losses = []
+        initial_encoder_grads = None
+        encoder_grads = None
+        decoder_grads = None
+
+        for i in range(self.model.num_particles):
+            loss = self.model.compute_branch_loss(batch)
+            final_losses.append(loss.item())
+            loss /= self.config["train"]["n_grad_accumulation_steps"]
+
+            grads = torch.autograd.grad(
+                loss,
+                list(self.model.initial_encoder.parameters())
+                + list(self.model.list_first_rats[i].parameters())
+                + list(self.model.encoder_rat_layers.parameters())
+                + list(self.model.decoder_rat_layers.parameters()),
+            )
+
+            initial_encoder_grads = grads[:initial_encoder_len]
+            particle_grads = grads[
+                initial_encoder_len : initial_encoder_len + particle_len
+            ]
+            encoder_grads = grads[
+                initial_encoder_len
+                + particle_len : initial_encoder_len
+                + particle_len
+                + encoder_len
+            ]
+            decoder_grads = grads[initial_encoder_len + particle_len + encoder_len :]
+
+            distance_nll[i, :] = torch.nn.utils.parameters_to_vector(particle_grads)
+
+        kernel_matrix, grad_kernel, _ = self.get_kernel(
+            params=params_matrix,
+            num_particles=self.config["train"]["n_grad_accumulation_steps"],
+        )
+
+        # Compute inner gradients with RPF kernel
+        # SVGD + prior_scale * params_matrix
+        # wSGLD_B
+        # encoders_grads = distance_nll - grad_kernel
+        encoders_grads = (1 / self.model.num_particles) * (
+            torch.matmul(kernel_matrix, distance_nll) - grad_kernel
+        )
+
+        # Copy inner_grads to main network
+        for i in range(self.model.num_particles):
+            for p_tar, p_src in zip(
+                self.model.list_first_rats[i].parameters(),
+                self.vector_to_list_params(
+                    encoders_grads[i], self.model.list_first_rats[i].parameters()
+                ),
+            ):
+                p_tar.grad.data.add_(p_src)  # TODO: divided by #samples if inner is BA
+
+        # Copy initial_encoder grads
+        for p_tar, p_src in zip(
+            self.model.initial_encoder.parameters(), initial_encoder_grads
+        ):
+            p_tar.grad.data.add_(
+                1 / self.model.num_particles * p_src
+                if p_src is not None
+                else torch.zeros_like(p_tar)
+            )
+
+        # Copy encoder grads
+        for p_tar, p_src in zip(
+            self.model.encoder_rat_layers.parameters(), encoder_grads
+        ):
+            p_tar.grad.data.add_(
+                1 / self.model.num_particles * p_src
+                if p_src is not None
+                else torch.zeros_like(p_tar)
+            )
+
+        # Copy decoder grads
+        for p_tar, p_src in zip(
+            self.model.decoder_rat_layers.parameters(), decoder_grads
+        ):
+            p_tar.grad.data.add_(
+                1 / self.model.num_particles * p_src
+                if p_src is not None
+                else torch.zeros_like(p_tar)
+            )
+
+        # TODO: Free GPU memory
+        return sum(final_losses) / self.model.num_particles
+
+    def _update(
+        self,
+        train_data_loader,
+        optimizer,
+        lr_scheduler,
+        scaler,
+        saver,
+        modeldir,
+        last_step,
+        prior_scale=1e-3,
+    ):
         # Counter for grad aggregation
         grad_accumulation_counter = 0
         losses = []
-        
+
         # 4. Start training loop
         with self.data_random:
             while True:
                 # Quit if too long
                 if last_step >= self.config["train"]["max_steps"]:
                     break
-                
+
                 # Compute and apply gradient
                 with self.model_random:
                     # Assert all params have grad
                     for p in self.model.parameters():
-                        p.grad = torch.zeros_like(p) if p.grad is None
-                    
-                    # Accumulate and update gradient 
+                        p.grad = torch.zeros_like(p) if p.grad is None else p.grad
+
+                    # Accumulate and update gradient
                     for _ in range(self.config["train"]["n_grad_accumulation_steps"]):
                         batch = next(train_data_loader)
-                        ensemble_train(batch, prior_scale)
-        
-        pass
-     
-    
+                        losses.append(self.ensemble_train(batch, prior_scale))
+
+                    # TODO: AMP Scaler?
+                    for param_group in optimizer.param_groups:
+                        torch.nn.utils.clip_grad_norm_(
+                            param_group["params"], self.config["train"]["clip_grad"]
+                        )
+
+                    optimizer.step()
+                    optimizer.zero_grad()
+                    lr_scheduler.update_lr(last_step)
+
+                last_step += 1
+
+                self._report(last_step=last_step, losses=losses, optimizer=optimizer)
+                self._evaluate(last_step=last_step, saver=saver, modeldir=modeldir)
+
+                # Reset the list of losses
+                losses = []
+
+
 def main(
-        args=None, logdir_suffix: List[str] = None, trainer_class: Type[Trainer] = Trainer
+    args=None, logdir_suffix: List[str] = None, trainer_class: Type[Trainer] = Trainer
 ):
     parser = argparse.ArgumentParser()
-    parser.add_argument("--logdir", required='output/xlmr-multilingual')
-    parser.add_argument("--config", default='configs/duorat/duorat-xlmr-multilingual.jsonnet')
-    parser.add_argument("--preproc_data_path", default='dataset/pkl/xlmr-multilingual') 
-    parser.add_argument("--load_path", default='')
-    parser.add_argument("--step", default='')
+    parser.add_argument("--logdir", required="output/xlmr-multilingual")
+    parser.add_argument(
+        "--config", default="configs/duorat/duorat-xlmr-multilingual.jsonnet"
+    )
+    parser.add_argument("--preproc_data_path", default="dataset/pkl/xlmr-multilingual")
+    parser.add_argument("--load_path", default="")
+    parser.add_argument("--step", default="")
     args = parser.parse_args(args)
 
     config = json.loads(_jsonnet.evaluate_file(args.config))
@@ -129,7 +336,7 @@ def main(
     logger = Logger(os.path.join(args.logdir, "log.txt"), reopen_to_flush)
     logger.log("Logging to {}".format(args.logdir))
     logger.log(f"Overwriting preproc save_path with: {args.preproc_data_path}")
-    
+
     if os.path.exists(args.preproc_data_path):
         logger.log("Skip preprocessing..")
     else:
@@ -142,10 +349,11 @@ def main(
     # Construct trainer and do training
     step = None if not args.step else int(args.step)
     trainer = trainer_class(logger, config)
-    if args.load_path != '':
+    if args.load_path != "":
         trainer.train(modeldir=args.logdir, load_path=args.load_path, step=step)
     else:
         trainer.train(modeldir=args.logdir, step=step)
+
 
 if __name__ == "__main__":
     main()
